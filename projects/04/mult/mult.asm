26898c63b5c9a612a3ded4c935ff6850570ed291
Eduardo Poleo <eduardopoleo@gmail.com>
2019-10-26 14:26:12 -0400

    Loop + sumation script in assembly language

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,38 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+@i
+M=0
+
+@R2
+M=0
+
+(LOOP)
+  @i
+  M = M + 1  // Increase i + 1
+
+  @R1
+  D = M      // sets D to R1
+
+  @i
+  D = M - D  // subtracts i - R1 
+
+  @END
+  D; JGT     // loops ends when i - R1 > 0 means that we already iterated R1 times
+
+  @R0
+  D = M      // sets D as R0
+
+  @R2
+  M = M + D  // Increases R2 by R0
+
+             // D = D - R1 This expression is incorrect R1 is not a var I have to access the
+             // value from memory I can't just do R1 I have to get it form MRM @R1 and then use it as M
+
+  @LOOP
+  0; JMP
+
+(END)
+  @END
+  0; JMP
